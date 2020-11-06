9bfbb01b6676de96e47bf9f83e89828b03c89952
gentle-expression-4880 <choim3@spu.edu>
2020-11-06 00:01:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,6 +4,22 @@ def find_even_values(src)
   # Output all even values in each nested array
   counter = 0;
   
-  while counter < src.length 
+  while counter < src.length do
+    src[counter]
+    
+    in_counter = 0
+    
+    while in_counter < src[counter] do
+      src[counter][in_counter]
+      if {
+        src[counter][in_counter] / 2 == 0
+        p src[counter][in_counter] }
+        in_counter += 1;
+      else 
+        in_counter += 1;
+      end
+      counter +=1
+    end
+        
 
 end
