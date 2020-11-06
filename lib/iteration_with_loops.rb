dda0a5899fafe6e54a702580666c8dc87b7af384
gentle-expression-4880 <choim3@spu.edu>
2020-11-06 00:10:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ def find_even_values(src)
     while in_counter < src[counter] do
       #src[counter][in_counter]
       if 
-        src[counter][in_counter] / 2 == 0
+        src[counter][in_counter] % 2 == 0
         p src[counter][in_counter] 
         in_counter += 1;
       else 
